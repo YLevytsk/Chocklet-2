0c9f4ecbd74474c9c0f503043da90553e4ae6f97
yLevytsk <Lupin773311asd@gmail.com>
2024-12-31 16:02:08 +0000

    нртт

--- js/overlay.js
+++ js/overlay.js
@@ -1,19 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const overlayButtons = document.querySelectorAll("[data-overlay]"); // Ищем элементы с data-overlay
+    const overlayButtons = document.querySelectorAll("[data-overlay]");
 
     overlayButtons.forEach(button => {
         button.addEventListener("click", () => {
-            const overlayId = button.getAttribute("data-overlay"); // Получаем значение data-overlay
-            const overlay = document.getElementById(overlayId); // Ищем связанный оверлей
+            const overlayId = button.getAttribute("data-overlay");
+            const overlay = document.getElementById(overlayId);
 
-            if (overlay.classList.contains("active")) {
-                overlay.classList.remove("active");
-            } else {
-                document.querySelectorAll(".overlay.active").forEach(openOverlay => {
-                    openOverlay.classList.remove("active");
-                });
-
-                overlay.classList.add("active");
+            if (overlay) {
+                overlay.classList.toggle("active");
             }
         });
     });
