34483c032e8e987df1fc398b0f26837bab6a4ae9
yLevytsk <Lupin773311asd@gmail.com>
2024-12-31 17:38:38 +0000

    авма

--- js/overlay.js
+++ js/overlay.js
@@ -1,16 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
     const overlayButtons = document.querySelectorAll("[data-overlay]");
-
+  
     overlayButtons.forEach(button => {
-        button.addEventListener("click", () => {
-            const wrapper = button.closest('.ingredients-item-wrapper');
-            
-            if (wrapper) {
-                wrapper.classList.toggle("flipped");
-            }
-        });
+      button.addEventListener("click", () => {
+        const wrapper = button.closest('.ingredients-item-wrapper');
+        
+        if (wrapper) {
+          wrapper.classList.toggle("flipped");
+        }
+      });
     });
-});
+  });
+  
 
 
   
