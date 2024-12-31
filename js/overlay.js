5ef2bcf8ac9bb484207262a78c64f33cc6673420
yLevytsk <Lupin773311asd@gmail.com>
2024-12-31 16:52:19 +0000

    uuy

--- js/overlay.js
+++ js/overlay.js
@@ -5,12 +5,17 @@ document.addEventListener("DOMContentLoaded", () => {
         button.addEventListener("click", () => {
             const overlayId = button.getAttribute("data-overlay");
             const overlay = document.getElementById(overlayId);
+            const wrapper = button.closest('.ingredients-item-wrapper'); // Находим контейнер карточки
 
             if (overlay) {
-                overlay.classList.toggle("active");
+                overlay.classList.toggle("active"); // Переключаем активный класс на оверлей
             }
+
+            // Переключаем класс для поворота карточки
+            wrapper.classList.toggle("active");
         });
     });
 });
+
 
   
